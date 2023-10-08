29fdbb197070d8e805678c57924e39771e5989fa
Luka_Hat <luka.hat58@gmail.com>
2023-10-08 23:10:42 +0200

    add navbar

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+
+// Define paths
+const srcDir = './'; // Source directory where your style.sass file is located
+const destDir = './'; // Destination directory where you want the compiled style.css to be saved
+
+// Compile Sass to CSS
+function compileSass() {
+  return src(`${srcDir}style.sass`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(dest(destDir))
+    .pipe(browserSync.stream());
+}
+
+// Watch for changes in Sass files
+function watchFiles() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+
+  watch(`${srcDir}style.sass`, compileSass);
+  watch('*.html').on('change', browserSync.reload);
+}
+
+// Define the default task
+exports.default = watchFiles;
